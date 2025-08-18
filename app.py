1f5dee1b403b4806bf6784c59c83f59b33e5a1f4
DharShan07vk <dharshanvk07@gmail.com>
2025-08-18 21:25:29 +0530

    Update CORS configuration to allow all origins for API requests

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
 
 UPLOAD_FOLDER = "static/uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
